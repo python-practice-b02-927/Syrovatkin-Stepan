b9ccf55a8454d46924519bf2a28ea6e7cc864c44
ssyrovatkin <55139429+ssyrovatkin@users.noreply.github.com>
2019-09-14 18:12:17 +0300

    Update task_22.py

--- lab1/task_22.py
+++ lab1/task_22.py
@@ -5,7 +5,6 @@ from pyrob.api import *
 
 @task
 def task_5_10():
-
 	while not wall_is_beneath():
 		while not wall_is_on_the_right():
 			fill_cell()
@@ -22,6 +21,7 @@ def task_5_10():
 				move_left()
 				fill_cell()
 			break
-	
+
+			
 if __name__ == '__main__':
     run_tasks()

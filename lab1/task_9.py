42f9afcdbe2f48881e6f741f36ac16fd8d4bf957
ssyrovatkin <55139429+ssyrovatkin@users.noreply.github.com>
2019-09-14 18:22:38 +0300

    Update task_9.py

--- lab1/task_9.py
+++ lab1/task_9.py
@@ -5,13 +5,13 @@ from pyrob.api import *
 
 @task
 def task_8_2():
-	while (wall_is_above() or wall_is_beneath()):
+	while not wall_is_on_the_right():
 		if not wall_is_above():
 			fill_cell()
 		move_right()	
-		if wall_is_on_the_right():
-			break
 	if not wall_is_above():
-			fill_cell()
+		fill_cell()
+
+
 if __name__ == '__main__':
     run_tasks()

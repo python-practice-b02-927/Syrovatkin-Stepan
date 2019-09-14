7c62fc908964bd5a23734a73119dd58cb17da112
ssyrovatkin <55139429+ssyrovatkin@users.noreply.github.com>
2019-09-14 17:23:52 +0300

    Update task_32.py

--- lab1/task_32.py
+++ lab1/task_32.py
@@ -7,26 +7,18 @@ from pyrob.api import *
 def task_8_18():
 	n=0
 	while wall_is_beneath():
-		while not wall_is_above():
-			move_up()
-			if cell_is_filled():
-				n+=1
-		while not wall_is_beneath():
-			move_down()
-		move_right()
-	while not wall_is_on_the_left():
-		move_left()
-	mov('ax',n)
-	while wall_is_beneath():
 		if wall_is_above() and wall_is_beneath():
 			fill_cell()
 		while not wall_is_above():
 			move_up()
+			if cell_is_filled():
+				n+=1
 			if not cell_is_filled():
 				fill_cell()
 		while not wall_is_beneath():
 			move_down()
 		move_right()
+	mov('ax',n)
 
 
 if __name__ == '__main__':

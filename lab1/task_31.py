fc7238564e6a16b3ba2efac4dd7693aaa416f477
ssyrovatkin <55139429+ssyrovatkin@users.noreply.github.com>
2019-09-15 16:37:39 +0300

    Update task_31.py

--- lab1/task_31.py
+++ lab1/task_31.py
@@ -15,7 +15,7 @@ def task_8_30():
 			move_left()
 		while not wall_is_beneath():
 			move_down()
-		if wall_is_beneath() and wall_is_on_the_left() and n=1:
+		if wall_is_beneath() and wall_is_on_the_left() and n==1:
 			break
 
 

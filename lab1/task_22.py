d3f18ecbf57a44bd86791d6f1ed776b3a689eb66
ssyrovatkin <55139429+ssyrovatkin@users.noreply.github.com>
2019-09-15 16:29:16 +0300

    Update task_22.py

--- lab1/task_22.py
+++ lab1/task_22.py
@@ -15,13 +15,14 @@ def task_5_10():
 			move_left()
 		if wall_is_beneath():
 			while not wall_is_on_the_right():
+				fill_cell()
 				move_right()
+				fill_cell()
 			while not wall_is_on_the_left():
-				fill_cell()
 				move_left()
-				fill_cell()
 			break
+	fill_cell()
 
-			
+
 if __name__ == '__main__':
     run_tasks()

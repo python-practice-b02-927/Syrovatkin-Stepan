f9e94c4fc9fb7280acafdcd7110b726489ceaff7
ssyrovatkin <55139429+ssyrovatkin@users.noreply.github.com>
2019-09-16 21:46:38 +0300

    Update Turtle_13.py

--- lab2/Turtle_13.py
+++ lab2/Turtle_13.py
@@ -1,37 +1,36 @@
 import turtle as t
-
+import math 
 t.shape('turtle')
 
 
 def duga(r):
-    for i in range(25):
-        t.forward(r)
-        t.right(360/50)
+    for i in range(180):
+        t.right(1)
+        t.forward(r*math.pi/180)
 
 
 def circle(r):
-    for i in range(50):
-        t.forward(r)
-        t.right(360/50)
-
+    for i in range(360):
+        t.right(1)
+        t.forward(r*math.pi/180)
 
 def eye():
 	t.pendown()
 	t.color('black','blue')
 	t.begin_fill()
-	circle(1.25)
+	circle(10)
 	t.end_fill()
 	t.penup()
 
 
 t.color('black','yellow')
 t.begin_fill()
-circle(8)
+circle(70)
 t.end_fill()
 t.penup()
-t.goto(-20,-25)
+t.goto(-25,-25)
 eye()
-t.goto(20,-25)
+t.goto(25,-25)
 eye()
 t.goto(0,-50)
 t.right(90)
@@ -39,10 +38,10 @@ t.pendown()
 t.pensize(8)
 t.forward(20)
 t.penup()
-t.goto(40,-60)
+t.goto(50,-70)
 t.pendown()
 t.color('red')
-duga(5)
+duga(50)
 
-
+input()
 

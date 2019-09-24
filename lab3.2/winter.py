3528ce8e353b8fcbaab689ee9f29fe7ac202e3c2
ssyrovatkin <Stepashan@bk.ru>
2019-09-24 13:45:10 +0300

    This programm has a lot off programms

---
+++ lab3.2/winter.py
@@ -0,0 +1,31 @@
+import graphics as gr
+
+win = gr.GraphWin("Jenkslex and Ganzz project", 1000, 600)
+
+def draw_snowman(win):
+	pass
+def draw_ice_hole(win):
+	pass
+def draw_fishing_rod(win):
+	pass
+def draw_fish(win):
+	pass
+def draw_star(win):
+	pass	
+	
+def main(win):
+	"""Draws picture"""
+	draw_snowman(win)
+	draw_ice_hole(win)
+	draw_fishing_rod(win)
+	draw_fish(win)
+	draw_star(win)
+	
+	
+	
+
+
+
+main(win)
+win.getMouse()
+win.close

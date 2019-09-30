67a36aebb01bc5f64a645c2568242cc0ca81e4af
ssyrovatkin <Stepashan@bk.ru>
2019-09-30 20:00:36 +0300

    added functions for smoke and wheels

--- lab3_2/winter.py
+++ lab3_2/winter.py
@@ -2,57 +2,51 @@ import graphics as gr
 
 win = gr.GraphWin("Jenkslex and Ganzz project", 600, 800)
 
+def draw_smoke(x1, y1, x2, y2):
+	smoke=gr.Oval(gr.Point(x1, y1), gr.Point(x2, y2))
+	smoke.setOutline(gr.color_rgb(230,230,230))
+	smoke.setFill(gr.color_rgb(230,230,230))
+	smoke.draw(win)
+	
+def draw_wheel(x1, y1, x2, y2):
+	wheel=gr.Oval(gr.Point(x1, y1), gr.Point(x2, y2))
+	wheel.setOutline(gr.color_rgb(0,0,0))
+	wheel.setFill(gr.color_rgb(0,0,0))
+	wheel.draw(win)
+	
 def draw_car(win):
 	
 	pipe=gr.Oval(gr.Point(160,595), gr.Point(320,605))
 	pipe.setOutline(gr.color_rgb(0,0,0))
 	pipe.setFill(gr.color_rgb(0,0,0))
 	pipe.draw(win)
-	
+
 	body1=gr.Rectangle(gr.Point(180,570), gr.Point(400,610))
 	body1.setOutline(gr.color_rgb(80,200,250))
 	body1.setFill(gr.color_rgb(80,200,250))
 	body1.draw(win)
-	
+
 	body2=gr.Rectangle(gr.Point(230,540), gr.Point(350,570))
 	body2.setOutline(gr.color_rgb(80,200,250))
 	body2.setFill(gr.color_rgb(80,200,250))
 	body2.draw(win)
-	
+
 	window1=gr.Rectangle(gr.Point(240,550), gr.Point(280,575))
 	window1.setOutline(gr.color_rgb(240,250,250))
 	window1.setFill(gr.color_rgb(240,250,250))
 	window1.draw(win)
-	
+
 	window2=gr.Rectangle(gr.Point(300,550), gr.Point(340,575))
 	window2.setOutline(gr.color_rgb(240,250,250))
 	window2.setFill(gr.color_rgb(240,250,250))
 	window2.draw(win)
-	
-	wheel1=gr.Oval(gr.Point(210,595), gr.Point(250,625))
-	wheel1.setOutline(gr.color_rgb(0,0,0))
-	wheel1.setFill(gr.color_rgb(0,0,0))
-	wheel1.draw(win)
-	
-	wheel2=gr.Oval(gr.Point(330,595), gr.Point(370,625))
-	wheel2.setOutline(gr.color_rgb(0,0,0))
-	wheel2.setFill(gr.color_rgb(0,0,0))
-	wheel2.draw(win)
-	
-	smoke1=gr.Oval(gr.Point(80,580), gr.Point(150,610))
-	smoke1.setOutline(gr.color_rgb(230,230,230))
-	smoke1.setFill(gr.color_rgb(230,230,230))
-	smoke1.draw(win)
-	
-	smoke2=gr.Oval(gr.Point(50,540), gr.Point(120,570))
-	smoke2.setOutline(gr.color_rgb(230,230,230))
-	smoke2.setFill(gr.color_rgb(230,230,230))
-	smoke2.draw(win)
-	
-	smoke3=gr.Oval(gr.Point(20,500), gr.Point(90,530))
-	smoke3.setOutline(gr.color_rgb(230,230,230))
-	smoke3.setFill(gr.color_rgb(230,230,230))
-	smoke3.draw(win)
+
+	draw_wheel(210, 595, 250, 625)
+	draw_wheel(330, 595, 370, 625)
+
+	draw_smoke(80, 580, 150, 610)
+	draw_smoke(50, 540, 120, 570)
+	draw_smoke(20, 500, 90, 530)
 	
 	
 	

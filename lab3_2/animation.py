b1c4132513c75018d5f17b70c751a30a073c7a8a
ssyrovatkin <Stepashan@bk.ru>
2019-10-07 19:56:03 +0300

    edited normal vector and operations with velocity, finished with changing velocity

--- lab3_2/animation.py
+++ lab3_2/animation.py
@@ -5,7 +5,7 @@ win = gr.GraphWin("Jenkslex and Ganzz project", 800, 1000)
 win.setBackground('purple')
 
 coords=gr.Point(200, 200)
-velocity=gr.Point(2,7)
+velocity=gr.Point(2,5)
 
 x0 = 400
 y0 = 325
@@ -18,7 +18,7 @@ def draw_sphere():
     sphere.setWidth(15)
 
 def draw_ball():
-    circle = gr.Circle(coords, 15)
+    circle = gr.Circle(coords, 20)
     circle.setFill('aqua')
     circle.draw(win)
     return circle
@@ -31,25 +31,34 @@ def add(point_1, point_2):
     return new_point    
 
 def r(x,y):
-    radius_vector = gr.Point((x0-x) / 275, (y0-y) / 275)
-    return r_vector
+    length_vector = sqrt((x0-x)**2 + (y0-y)**2)
+    radius_vector = gr.Point((x0-x) / length_vector, (y0-y) / length_vector)
+    return radius_vector
 
 def n(x,y):
-    normal_vector = gr.Point((x0-x) / 275, (y0-y) / 275)
-    return r_vector
+    length_vector = sqrt((x0-x)**2 + (y0-y)**2)
+    normal_vector = gr.Point((y0-y) / length_vector, -(x0-x) / length_vector)
+    return normal_vector
 
 def scalar_multiplication(vector_1, vector_2):
     scalar = vector_1.x*vector_2.x + vector_1.y*vector_2.y
     return scalar
 
 def velocity_changing(velocity, x, y):
-    vel_normal_0 = r(x,y)*scalar_multiplication(r(x,y), velocity)
-    vel_tau = n(x,y)*scalar_multiplication(n(x,y), velocity)
-    vel_normal_1 = -vel_normal_0
+    vel_normal_0 = gr.Point(r(x,y).x*scalar_multiplication(r(x,y), velocity), r(x,y).y*scalar_multiplication(r(x,y), velocity))
+    vel_tau = gr.Point(n(x,y).x*scalar_multiplication(n(x,y), velocity), n(x,y).y*scalar_multiplication(n(x,y), velocity))
+    vel_normal_1 = gr.Point(-vel_normal_0.x, -vel_normal_0.y)
     
     i = gr.Point(1, 0)
     j = gr.Point(0, 1)
     
+    vel_1_x = scalar_multiplication (vel_normal_1, i)
+    vel_2_x = scalar_multiplication (vel_tau, i)
+    
+    vel_1_y = scalar_multiplication (vel_normal_1, j)
+    vel_2_y = scalar_multiplication (vel_tau, j)
+    
+    velocity = gr.Point(vel_1_x + vel_2_x, vel_1_y + vel_2_y) 
     
     return velocity
 
